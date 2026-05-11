29677122fdad84e80d68798c6907a9fe9a58a184
mladengav <mladen-g@hotmail.com>
2026-05-11 11:46:31 -0400

    Add testing and stub for architecure tests.

--- test/architecture_test.py
+++ test/architecture_test.py
@@ -1,19 +1,44 @@
-from pytestarch import get_evaluable_architecture, DEFAULT_EXCLUSIONS, LayeredArchitecture
-from pathlib import Path
+# TODO:  Disabled because pytestarch expects a subfolder, e.g. "fincol" under the main "src" folder.  Tests can be enabled when/if project is
+#        reorganized according to that structure.
 
-PROJECT_ROOT = Path(__file__).resolve().parent
-EXCLUSIONS = DEFAULT_EXCLUSIONS + ("cache", "test", "testcache", "testdata")
+# import pytest
+# from pytestarch import EvaluableArchitecture, LayeredArchitecture, get_evaluable_architecture, LayerRule
 
-evaluable = get_evaluable_architecture(PROJECT_ROOT, PROJECT_ROOT / "src")
+# @pytest.fixture(scope="session")
+# def evaluable(pytestconfig: pytest.Config) -> EvaluableArchitecture:
+#     project_root = pytestconfig.rootpath
+#     src_path = project_root / "src"
+#     return get_evaluable_architecture(str(src_path), "")
 
-arch = (
-    LayeredArchitecture() 
-    .layer("domain") 
-    .have_modules_with_names_matching("domain.*")
-    .layer("application") 
-    .have_modules_with_names_matching("application.*")
-    .layer("infrastructure") 
-    .have_modules_with_names_matching("infrastructure.*")
-    .layer("presentation_cli") 
-    .have_modules_with_names_matching("presentation_cli.*")
-)
+# @pytest.fixture(scope="session")
+# def layered_architecture() -> LayeredArchitecture:
+#     return (
+#         LayeredArchitecture()
+#         .layer("domain")
+#         .containing_modules(["src.domain"])
+#         .layer("application")
+#         .containing_modules(["src.application"])
+#         .layer("infrastructure")
+#         .containing_modules(["src.infrastructure"])
+#         .layer("presentation_cli")
+#         .containing_modules(["src.presentation_cli"])
+#     )
+
+
+# def test_domain_cannot_reference_higher_layers(
+#     evaluable: EvaluableArchitecture, layered_architecture: LayeredArchitecture
+# ) -> None:
+#     """
+#     Domain layer must not reference higher layers.
+#     """
+#     rule = (
+#         LayerRule()
+#         .based_on(layered_architecture)
+#         .layers_that()
+#         .are_named("domain")
+#         .should_not()
+#         .access_layers_that()
+#         .are_named(["application", "infrastructure"])
+#     )
+
+#     rule.assert_applies(evaluable)
